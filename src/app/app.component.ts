f1127cb1efa3c11fc054d6c1a2032f146f0b36e1
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2018-04-22 22:59:18 -0400

    Add fixed/fluid layout option to app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  isFluid: boolean = false;
+  newLayout: string = 'fluid';
+
+  ngOnInit() {}
+
+  switchLayout() {
+    if (this.isFluid) {
+      this.isFluid = false;
+      this.newLayout = 'fluid';
+    }
+    else {
+      this.isFluid = true;
+      this.newLayout = 'fixed';
+    }
+  }
 }
