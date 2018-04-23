324273379bb801d4803d79f7b47ef2cce09fb32d
Sergey Golitsynskiy <sgolitsynskiy@gmail.com>
2018-04-23 00:20:27 -0400

    Update production environment.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,23 +2,29 @@ export const environment = {
   production: true,
   pagination: {
     headers: {
-      totalRecords : 'Pagination-Total-Records',
-      firstRecord  : 'Pagination-First-Record',
-      lastRecord   : 'Pagination-Last-Record', 
-      totalPages   : 'Pagination-Total-Pages', 
-      pageSize     : 'Pagination-Page-Size',
-      pageNumber   : 'Pagination-Page-Number' 
+      totalRecords      : 'Pagination-Total-Records',
+      pageSize          : 'Pagination-Page-Size',
+      totalPages        : 'Pagination-Total-Pages', 
+      firstRecord       : 'Pagination-First-Record',
+      lastRecord        : 'Pagination-Last-Record', 
+      currentPageNumber : 'Pagination-Current-Page-Number', 
+      currentPageSize   : 'Pagination-Current-Page-Size' 
     },
     pageNumberParameter: 'page',
     pageSizeParameter: 'pagesize'
   },
   collections: {
     columns: 12,
-    pathToFeatured: 'TODO',
-    imgSuffix: '_f.jpg'
+    pathToFeatured: 'http://localhost/fortepan/featured_600x450/',
+    imgSuffix: '_600x450.jpg'
   },
   items: {
     columns: 12,
-    imgSuffix: '.jpg'
+    imgSuffix:     '.jpg',
+    pathTo75:      'http://localhost/fortepan/thumbs/',
+    pathTo400:     'http://localhost/fortepan/h400/',
+    pathTo600:     'http://localhost/fortepan/w600/',
+    pathTo700:     'http://localhost/fortepan/h700/',
+    pathToOriginal: 'http://localhost/fortepan/originals/'
   }
 };
